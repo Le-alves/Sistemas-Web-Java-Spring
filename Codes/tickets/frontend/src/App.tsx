03518876ba120aeb2b949e4d7c64da0fe3ce949f
Fernando B Oliveira <fboliveira25@gmail.com>
2026-02-03 10:21:52 -0300

    updated: change to app routes

--- Codes/tickets/frontend/src/App.tsx
+++ Codes/tickets/frontend/src/App.tsx
@@ -1,16 +1,13 @@
+import AppHeader from "./components/AppHeader"
 import { Button } from "./components/ui/button"
-import ListUsers from "./pages/users/ListUsers"
 
 function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold">Sistema de Vendas de Tickets</h1>
+      <AppHeader title="Página principal" />
 
       <Button>Login</Button>
-
-      <ListUsers />
-
 
     </>
   )
